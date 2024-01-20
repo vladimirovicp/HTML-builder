67526deca853a117e037a4166aaa337d4b803d02
vladimirovicp <vladimirovicp70@gmail.com>
2024-01-20 18:17:24 +0400

    feat: add size to kb

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,9 +1,6 @@
-const fs = require('fs');
-const path = require('path')
+const fs = require('node:fs');
+const path = require('node:path')
 
-// https://www.geeksforgeeks.org/node-js-fs-readdir-method/
-
-//В этом примере используется метод fs.readdir() для возврата имен файлов или файловых объектов в каталоге.
 fs.readdir(path.join(__dirname, 'secret-folder'), (err, files) => {
   if (err)
     console.log(err);
@@ -16,32 +13,14 @@ fs.readdir(path.join(__dirname, 'secret-folder'), (err, files) => {
           console.error(err);
           return;
         }
-        //stats.isFile(); // true
-
         if(stats.isFile()){
-
-          //const filename = path.parse(`secret-folder/${file}`).name;
-          //const ext = path.parse(`secret-folder/${file}`).ext;
           const ext = file.split('.').pop();
           const filename = file.split('.').shift();
-          const size = stats.size;
-          console.log(`${filename} - ${ext} - ${size}`);
-
-          //console.log(stats.size);
+          const size = Math.ceil(stats.size/ 1024); // kb
+          //Примечание: округление размера файла не требуется; преобразование в кБ необязательно!
+          console.log(`${filename} - ${ext} - ${size}kb`);
         }
-
-        // stats.isDirectory(); // false
-        // stats.isSymbolicLink(); // false
-        // stats.size; // 1024000 //= 1MB
-        //
-        // console.log(stats.size)
-
       });
-
-
-      //console.log(file);
-
-
     })
   }
 })
