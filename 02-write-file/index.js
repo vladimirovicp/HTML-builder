33dbfca41bb07536490639fc485e29bdd98c222a
vladimirovicp <vladimirovicp70@gmail.com>
2025-01-21 16:58:21 +0400

    fix: закрытие по ctrl + с

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -13,8 +13,15 @@ stdout.write('Hello! Please enter text...\n');
 
 rl.on('line', (comand) =>{
     if(comand === 'exit'){
-        stdout.write('Happily!\n');
+        stdout.write('\nHappily!\n');
         rl.close();
     }
     stream.write(comand + '\n');
+});
+
+rl.input.on('keypress', (char, key) => {
+  if (key && key.name === 'c' && key.ctrl) {
+    process.stdout.write('Happily!\n');
+    rl.close();
+  }
 });
